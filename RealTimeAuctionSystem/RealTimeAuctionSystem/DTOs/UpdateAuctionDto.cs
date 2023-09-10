46ccd8c4f9c1e9ff1e02a5cb7a8eb267235c6e23
Jure Bakula <jurebakula98@gmail.com>
2023-09-10 20:35:41 +0200

    fixxx

--- RealTimeAuctionSystem/RealTimeAuctionSystem/DTOs/UpdateAuctionDto.cs
+++ RealTimeAuctionSystem/RealTimeAuctionSystem/DTOs/UpdateAuctionDto.cs
@@ -6,8 +6,8 @@
         public string? Description { get; set; } = string.Empty;
         public float StartingPrice { get; set; }
         public int CategoryId { get; set; }
-        public string StartsAt { get; set; }
-        public string EndsIn { get; set; }
+        public string StartsAt { get; set; } = string.Empty;
+        public string EndsIn { get; set; } = string.Empty;
         public IFormFile? Image { get; set; }
     }
 }
